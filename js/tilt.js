3faea172f8f21b48741ca9d51cb0950b8a7f2296
arjay <arjay@nxt.work>
2022-11-28 08:00:02 +0800

    update card slider and add map and news

---
+++ js/tilt.js
@@ -0,0 +1,9 @@
+VanillaTilt.init(document.querySelector(".admission-container"), {
+  max: 25,
+  speed: 400,
+});
+
+VanillaTilt.init(document.querySelector(".mobile-container"), {
+  max: 25,
+  speed: 400,
+});
